3b4f8e8734ad9fc6f2b77ed442bf678f711d7a13
Jacob Baker <jb00645@my.westga.edu>
2025-09-20 18:48:20 -0400

    Finished Index and Contact Details
    Created the Index and the Contacts Page, not able to test yet since no controllers are created, however, data has been added in the data base.

--- BakerAddressBook/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BakerAddressBook.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
