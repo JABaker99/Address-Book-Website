d34c7f8a8fe7e7b108657c8be56b5b5c21165965
Jacob Baker <jb00645@my.westga.edu>
2025-09-20 15:30:18 -0400

    Created the Contact and Category Model
    Added the data base components, the Categories and the contact tables. I also was able to add the DB NuGet Package Manager in as well.

---
+++ BakerAddressBook/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace BakerAddressBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public IList<Contact> Contacts { get; set; }
+    }
+}
---
+++ BakerAddressBook/Models/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BakerAddressBook.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        public string Nickname { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+    }
+}
