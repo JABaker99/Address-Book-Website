0509c584f41714d72d922a6644f066ad674dbf1d
Jacob Baker <jb00645@my.westga.edu>
2025-09-21 00:01:40 -0400

    Created Delete View
    Created and IMplemneted the Delete View of contacts, which removes the contact and returns to the home page once done.

--- BakerAddressBook/Controllers/ContactsController.cs
+++ BakerAddressBook/Controllers/ContactsController.cs
@@ -27,5 +27,32 @@ namespace BakerAddressBook.Controllers
 
             return View(contact);
         }
+
+        // GET: /contacts/delete/5/
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Contact contact = await _context.Contacts.Include(c => c.Category)
+                .FirstOrDefaultAsync(c => c.ContactId == id.Value);
+            if (contact == null) return NotFound();
+
+            return View(contact);
+        }
+
+        // POST: /contacts/delete/5/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Contact contact = await _context.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                _context.Contacts.Remove(contact);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
