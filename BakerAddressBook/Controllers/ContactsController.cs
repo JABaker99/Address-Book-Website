dfb11895ca255582df68396742a6bd7b78183ae2
Jacob Baker <jb00645@my.westga.edu>
2025-09-20 20:33:45 -0400

    Implmented Details
    Created the view and also made the ContactController handle the on click.

--- BakerAddressBook/Controllers/ContactsController.cs
+++ BakerAddressBook/Controllers/ContactsController.cs
@@ -1,6 +1,31 @@
-﻿namespace BakerAddressBook.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BakerAddressBook.Data;
+using BakerAddressBook.Models;
+
+namespace BakerAddressBook.Controllers
 {
-    public class ContactsController
+    public class ContactsController : Controller
     {
+        private readonly BakerAppDbContext _context;
+
+        public ContactsController(BakerAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /contacts/details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var contact = await _context.Contacts
+                .Include(c => c.Category)
+                .FirstOrDefaultAsync(c => c.ContactId == id);
+
+            if (contact == null) return NotFound();
+
+            return View(contact);
+        }
     }
 }
